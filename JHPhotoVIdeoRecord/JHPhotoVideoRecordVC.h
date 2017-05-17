e4b232c5e92de8761608c032e63884c7b154a2e1
Gjianhao <kevinbina@163.com>
2017-05-17 23:10:33 +0800

    一些注释

--- JHPhotoVIdeoRecord/JHPhotoVideoRecordVC.h
+++ JHPhotoVIdeoRecord/JHPhotoVideoRecordVC.h
@@ -8,6 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ 录制模式
+
+ - GPUImageShowStyleOnlyPhoto: 只进行拍照
+ - GPUImageShowStyleOnlyVideo: 只进行摄像
+ - GPUImageShowStyleAll: 两者共存
+ */
 typedef NS_ENUM(NSUInteger, GPUImageShowStyle) {
     GPUImageShowStyleOnlyPhoto = 0,
     GPUImageShowStyleOnlyVideo,
@@ -32,7 +39,7 @@ typedef NS_ENUM(NSUInteger, LZVideoRecordOrientation) {
 @interface JHPhotoVideoRecordVC : UIViewController
 
 @property (nonatomic, assign) id <JHRecorderVideoDelegate> delegate;
-
+/* 录制模式 */
 @property (nonatomic, assign) GPUImageShowStyle showStyle;
 
 @end
